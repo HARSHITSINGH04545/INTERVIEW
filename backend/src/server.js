9d54b2f5ab921bd741bef671d7cd58b9d5443412
Harshit Singh <masterspy4545@gmail.com>
2026-02-03 12:32:06 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+const app = express ( ) ;
+
+console. log(ENV.PORT) ;
+console. log(ENV.DB_URL) ;
+app.get("/health", (req, res) => {
+res.json("sapi is up and runing" );
+})
+app. listen(ENV. PORT, () => console. log("Server is running on port:", ENV.PORT) );
