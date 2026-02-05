b9af5c1fb739a61cc250d80d3ade0c8e7735de16
Harshit Singh <masterspy4545@gmail.com>
2026-02-05 12:02:35 +0530

    modifying db setup

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -10,7 +10,9 @@ export const connectDB = async () => {
 
 
 try {
-
+if(!ENV.DB_URL){
+    throw new Error("DB_URL is not ddefined in environment variables");
+}
 const conn = await mongoose.connect(ENV.DB_URL);
 
 console.log(" Connected to MongoDB:", conn.connection.host);
