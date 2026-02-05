2a45ff8678e42f67aea5551cd201d3d4cc32158f
Harshit Singh <masterspy4545@gmail.com>
2026-02-05 11:51:45 +0530

    db and clerk setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 const app = express ( ) ;
 
 console. log(ENV.PORT) ;
@@ -7,4 +8,26 @@ console. log(ENV.DB_URL) ;
 app.get("/health", (req, res) => {
 res.json("sapi is up and runing" );
 })
-app. listen(ENV. PORT, () => console. log("Server is running on port:", ENV.PORT) );
+const startServer = async () => {
+
+try {
+
+await connectDB();
+
+app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+
+} catch (error) {
+
+console.error("* Error starting the server", error)
+
+}
+
+};
+
+
+
+startServer();
+
+
+    
+
